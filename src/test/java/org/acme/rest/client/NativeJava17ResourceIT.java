79f2d3b5279f61d0675d6bc10f437b671d9da85e
Fernando Balieiro (CTW) <Fernando.Balieiro@ctw.bmwgroup.com>
2021-11-01 09:46:41 +0000

    Project setup using Java 17.

---
+++ src/test/java/org/acme/rest/client/NativeJava17ResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.rest.client;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeJava17ResourceIT extends Java17ResourceTest {
+
+    // Execute the same tests but in native mode.
+}
